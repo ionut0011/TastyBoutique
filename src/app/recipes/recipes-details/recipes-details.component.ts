01fd2056f8ac0631c0e5399dc880522b0cf3f295
Ioana Iftime <iftimeioana1@gmail.com>
2020-07-27 13:49:42 +0300

    pagina adaugare

--- src/app/recipes/recipes-details/recipes-details.component.ts
+++ src/app/recipes/recipes-details/recipes-details.component.ts
@@ -1,15 +1,71 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {FormControl} from '@angular/forms';
+
 
 @Component({
   selector: 'app-recipes-details',
   templateUrl: './recipes-details.component.html',
   styleUrls: ['./recipes-details.component.css']
 })
-export class RecipesDetailsComponent implements OnInit {
+export class RecipesDetailsComponent implements OnInit
+{
+  fileToUpload: any;
+  imageUrl: any;
+  name: string;
+  description: string;
+  isPrivate: boolean;
+  isAdmin: boolean;
+  isEditing: boolean;
+  filterss = new FormControl();
+  filtersList: string[] = ['Gluten free', 'Vegan', 'Sugar free'];
+  ingredients:string[] =[];
+  photos: Blob[] = [];
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    if (this.router.url === '/create-recipe') {
+      this.isEditing = true;
+    }
+    else {
+      this.name = "Add recipes";
+      this.description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+      this.isEditing = false;
+    }
+    this.isPrivate = true;
+    this.isAdmin = true;
+
+  }
+
+
+
+  startUpdating() {
+    this.isEditing = true;
+  }
+
+  save() {
+    this.photos.push(this.imageUrl);
+    this.imageUrl = null;
+    this.isEditing = false;
+  }
+
+
+
+  addIngredients(newIngredient: string) {
+    if (newIngredient!="") {
+      this.ingredients.push(newIngredient);
+    }
+  }
+
+  handleFileInput(file: FileList) {
+    this.fileToUpload = file.item(0);
+
+    let reader = new FileReader();
+    reader.onload = (event: any) => {
+      this.imageUrl = event.target.result;
+    }
+    reader.readAsDataURL(this.fileToUpload);
   }
 
 }
