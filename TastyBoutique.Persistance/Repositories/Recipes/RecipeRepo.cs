b80bf8b79c53957f61406e0669e982aa170fbd68
ionut0011 <ioan.buhai@student.tuiasi.ro>
2020-07-27 15:11:04 +0300

    full-merged

--- TastyBoutique.Persistance/Repositories/Recipes/RecipeRepo.cs
+++ TastyBoutique.Persistance/Repositories/Recipes/RecipeRepo.cs
@@ -33,5 +33,9 @@ namespace TastyBoutique.Persistance.Recipes
         public async Task<Models.Ingredients> GetByName(string Name)
             => await this.context.Ingredients.Where(i => i.Name.Equals(Name)).FirstAsync();
 
+        public Task<Models.Recipes> GetByIdWithComments(Guid id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
