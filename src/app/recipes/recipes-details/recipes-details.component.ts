4eece005b623cfe3f6e4aea5b6e8e58f4dd27359
paula fechet <pioana027@gmail.com>
2020-07-28 18:20:11 +0300

    recipe list

--- src/app/recipes/recipes-details/recipes-details.component.ts
+++ src/app/recipes/recipes-details/recipes-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router'
 
 @Component({
   selector: 'app-recipes-details',
@@ -7,9 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesDetailsComponent implements OnInit {
 
-  constructor() { }
+  fileToUpload :any;
+  imageUrl : any;
+
+  name: string;
+  description: string;
+  isPrivate: boolean;
+  isAdmin:boolean;
+  isEditing: boolean;
+  photos: Blob[] = [];
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    if (this.router.url === '/create-recipe') {
+      this.isEditing = true;
+    } else {
+      this.name = "My recipe";
+      this.description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+      this.isEditing = false;
+    }
+    this.isPrivate = true;
+    this.isAdmin = true;
+  }
+
+  startUpdating() {
+    this.isEditing = true;
+  }
+
+  save() {
+    this.photos.push(this.imageUrl);
+    this.imageUrl = null;
+    this.isEditing = false;
+  }
+
+  handleFileInput(file: FileList) {
+    this.fileToUpload = file.item(0);
+
+    let reader = new FileReader();
+    reader.onload = (event: any) => {
+      this.imageUrl = event.target.result;
+    }
+    reader.readAsDataURL(this.fileToUpload);
   }
 
 }
--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,15 +1,68 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Recipes } from '../models/recipes';
 
 @Component({
-  selector: 'app-recipes-list',
+  selector: 'app-recipe-list',
   templateUrl: './recipes-list.component.html',
-  styleUrls: ['./recipes-list.component.css']
+  styleUrls: ['./recipes-list.component.css'],
 })
 export class RecipesListComponent implements OnInit {
+  public recipesList: Recipes[];
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
+    this.recipesList = [
+      {
+        id: '1',
+        name: 'Mancarica de cartofi',
+        description:
+          'Delicios',
+        backgroundImage: '../../assets/images/food.jpg',
+        isPrivate: false,
+      },
+
+      {
+        id: '2',
+        name: 'Clatite',
+        description: 'Yummy',
+        backgroundImage: '../../assets/images/food.jpg',
+        isPrivate: false,
+      },
+
+      {
+        id: '3',
+        name: 'Smoothie cu banana',
+        description:
+          'Cea mai buna bautura',
+        backgroundImage: '../../assets/images/food.jpg',
+        isPrivate: false,
+      },
+      {
+        id: '4',
+        name: 'Supa de cartofi',
+        description:
+          '',
+        backgroundImage: '../../assets/images/food.jpg',
+        isPrivate: false,
+      },
+
+      {
+        id: '5',
+        name: 'Sarmale',
+        description: 'E foarte frumos aici sa mai venim. Briza e minunata',
+        backgroundImage: '../../assets/images/food.jpg',
+        isPrivate: false,
+      },
+
+
+    ];
   }
 
+  goToRecipe(id: string): void {
+    console.log(id);
+    this.router.navigate(['/recipes/details']);
+  }
 }
