79d0d7530c2c353101abf77506b4a8c1a3914be7
paula fechet <pioana027@gmail.com>
2020-08-04 20:42:55 +0300

    comment get and post

---
+++ src/app/shared/stars-review/stars-review.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-stars-review',
+  templateUrl: './stars-review.component.html',
+  styleUrls: ['./stars-review.component.css']
+})
+export class StarsReviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
