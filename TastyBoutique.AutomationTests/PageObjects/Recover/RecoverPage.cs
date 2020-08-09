58708c84bdfa56ca7defa887e53ccd94b9066afd
epicrecords98 <61226184+epicrecords98@users.noreply.github.com>
2020-08-09 11:56:49 +0300

    -added automated test for password change/recovery

---
+++ TastyBoutique.AutomationTests/PageObjects/Recover/RecoverPage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace TastyBoutique.AutomationTests.PageObjects.Recover
+{
+    public class RecoverPage :BasePage
+    {
+
+        public RecoverPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(this, new RetryingElementLocator(this.driver, TimeSpan.FromSeconds(20)));
+        }
+        #region Recover
+        [FindsBy(How = How.Name, Using = "email")]
+        public IWebElement TxtEmail { get; set; }
+
+        [FindsBy(How = How.Name, Using = "newpassword")]
+        public IWebElement TxtNewPassword { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "[type='button']")]
+        public IWebElement BtnChangePswd { get; set; }
+
+
+        [FindsBy(How = How.CssSelector, Using = ".toast-message")]
+        public IWebElement ErrInvalidRec { get; set; }
+        #endregion
+
+        public void Recover(string email, string password)
+        {
+            TxtEmail.SendKeys(email);
+            TxtNewPassword.SendKeys(password);
+            BtnChangePswd.Click();
+        }
+    }
+}
---
+++ TastyBoutique.AutomationTests/Tests/RecoverTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TastyBoutique.AutomationTests.Helpers;
+using TastyBoutique.AutomationTests.PageObjects.Dashboard;
+using TastyBoutique.AutomationTests.PageObjects.Login;
+using TastyBoutique.AutomationTests.PageObjects.Recover;
+using Xunit;
+
+namespace TastyBoutique.AutomationTests.Tests
+{
+    public class RecoverTests : Browser, IDisposable
+    {
+        public LoginPage loginPage;
+        public DashboardPage dashboardPage;
+        public RecoverPage recoverPage;
+        public RecoverTests() : base()
+        {
+            Driver.Navigate()
+                .GoToUrl("http://www.tastyboutique.tk/?fbclid=IwAR2lyNONegXkpDwizPAWYMh0zwiEDG9Ybtje6LZ_hlWjPx-gVYuSFJKe5P8#/recover");
+            recoverPage = new RecoverPage(Driver);
+        }
+
+        [Fact]
+        public void Recover_With_Valid_Credentials()
+        {
+            recoverPage.Recover("chelaru.george1998@yahoo.com","Serioux33");
+            loginPage = new LoginPage(Driver);
+            loginPage.WaitForPageToLoad(".toast-message");
+            loginPage.Login("chelaru.george1998@yahoo.com", "Serioux33");
+            dashboardPage = new DashboardPage(Driver);
+            dashboardPage.WaitForPageToLoad(".toast-message");
+            Assert.True(dashboardPage.LabelLoginSuccessfull.Displayed);
+        }
+
+        [Fact]
+        public void Recover_With_Invalid_Email()
+        {
+            recoverPage.Recover("asdasda@yahoo.com", "TestAnonim123");
+            recoverPage.WaitForPageToLoad(".toast-message");
+            Assert.Contains("Something went wrong", recoverPage.ErrInvalidRec.Text);
+        }
+
+        [Fact]
+        public void Recover_With_Invalid_Password()
+        {
+            recoverPage.Recover("chelaru.george1998@yahoo.com", "test");
+            recoverPage.WaitForPageToLoad(".toast-message");
+            Assert.Contains("Something went wrong", recoverPage.ErrInvalidRec.Text);
+        }
+        public void Dispose()
+        {
+            CloseBrowser();
+        }
+    }
+}
