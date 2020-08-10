00922718b09d4cb068d1f3fbd18c945b2e7f2093
epicrecords98 <61226184+epicrecords98@users.noreply.github.com>
2020-08-11 01:20:03 +0300

    -added automated test for adding a comment to a recipe within the recipe list

---
+++ TastyBoutique.AutomationTests/PageObjects/RecipeList/RecipeListPage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace TastyBoutique.AutomationTests.PageObjects.RecipeList
+{
+    public class RecipeListPage:BasePage
+    {
+        public RecipeListPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(this, new RetryingElementLocator(this.driver, TimeSpan.FromSeconds(10)));
+        }
+
+        [FindsBy(How = How.XPath, Using = "//app-root/app-recipe-list/div[@class='container']/div[2]/div[1]/div[@class='card']/div[@class='text']/div[@class='btn lets-cook-btn']")]
+        public IWebElement BtnLetsCook { get; set; }
+
+
+        [FindsBy(How = How.XPath, Using = "//app-root/app-recipes-details/div[@class='d-flex justify-content-center justify-content-md-center']/section[@class='form-section']/div[@class='comments-section']/form//input[@type='text']")]
+        public IWebElement TxtComment { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = ".create_new_comment > .stars")]
+        public IWebElement Stars { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//app-root/app-recipes-details[@class='ng-star-inserted']//section[@class='form-section']/div[@class='comments-section']/form//button[@type='button']/span[.='Add comment']")]
+        public IWebElement BtnAddComment { get; set; }
+
+        [FindsBy(How=How.CssSelector,Using= ".toast-message")]
+        public IWebElement CommentPosted { get; set; }
+
+
+        public void AddComment(string comment)
+        {
+            BtnLetsCook.Click();
+            Stars.Submit();
+            TxtComment.SendKeys(comment);
+            BtnAddComment.Click();
+        }
+    }
+}
---
+++ TastyBoutique.AutomationTests/Tests/RecipeListTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TastyBoutique.AutomationTests.Helpers;
+using TastyBoutique.AutomationTests.PageObjects.Dashboard;
+using TastyBoutique.AutomationTests.PageObjects.Login;
+using TastyBoutique.AutomationTests.PageObjects.RecipeList;
+using Xunit;
+
+namespace TastyBoutique.AutomationTests.Tests
+{
+    public class RecipeListTests : Browser, IDisposable
+    {
+        public LoginPage loginPage;
+        public DashboardPage dashboardPage;
+        public RecipeListPage recipeListPage;
+        public RecipeListTests() : base()
+        {
+            Driver.Navigate()
+                .GoToUrl("http://www.tastyboutique.tk/#/login");
+            loginPage = new LoginPage(Driver);
+        }
+
+        [Fact]
+        public void Leave_A_Comment_On_A_Recipe()
+        {
+            loginPage.Login("testing123@yahoo.com", "Serioux44");
+            dashboardPage = new DashboardPage(Driver);
+            dashboardPage.ToSeeRecipies();
+            recipeListPage = new RecipeListPage(Driver);
+            recipeListPage.AddComment("Exceptional");
+            Assert.True(recipeListPage.CommentPosted.Displayed);
+        }
+
+       
+        public void Dispose()
+        {
+            CloseBrowser();
+        }
+    }
+
+}
+
